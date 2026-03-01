d0525b9dc3a6c17a5fd8129153582e80b2a79dae
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-03-01 12:15:57 +0545

    Connecting to MongoDB and setting up Express server.

---
+++ server/config/connectDb.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL
+    );
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log(`Database connection error: ${error}`);
+  }
+};
+
+export default connectDb;
