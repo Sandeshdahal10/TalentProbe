273423f9b963b315ae2b92071923e1a1120ce191
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-03-02 09:02:51 +0545

    Fetched the data to get current User.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Auth from './pages/Auth.jsx'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 export const ServerUrl = "https://talentprobe.onrender.com"
 function App() {
+  useEffect(()=> {
+    const getUser = async () => {
+      try {
+        const result = await axios.get(ServerUrl + "/api/user/currentUser",{
+          withCredentials: true
+        })
+        console.log(result.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getUser();
+  },[])
   return (
     <Routes>
       <Route path='/' element={<Home/>} />
