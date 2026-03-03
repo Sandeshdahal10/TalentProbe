7d6618618616f3b13a7a6b549c446b9a205c59e6
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-03-03 07:50:46 +0545

    Changes the import of framer-motion.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import { useSelector } from "react-redux";
-import { motion } from "motion/react";
+import { motion } from "framer-motion";
 import {
   BsRobot,
   BsMic,
