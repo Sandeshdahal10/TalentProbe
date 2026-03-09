a4fdd166f3e0aec487cae6e80924b7631246ed3d
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-03-09 19:07:38 +0545

    Fetched the generate question api and implemented the question display and answer recording functionality. Also added a video component to play the interview questions.

--- client/src/components/Step1SetUp.jsx
+++ client/src/components/Step1SetUp.jsx
@@ -10,7 +10,12 @@ import {
 import axios from "axios";
 import { ServerUrl } from "../App";
 
+import { useDispatch, useSelector } from "react-redux";
+import { setUserData } from "../redux/userSlice";
+
 function Step1SetUp({ onStart }) {
+  const { userData } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
   const [role, setRole] = useState("");
   const [experience, setExperience] = useState("");
   const [mode, setMode] = useState("Technical");
@@ -43,6 +48,35 @@ function Step1SetUp({ onStart }) {
       setAnalyzing(false);
     } catch (error) {
       console.log(error);
+    }
+  };
+
+  const handleStart = async () => {
+    setLoading(true);
+    try {
+      const result = await axios.post(
+        ServerUrl + "/api/interview/generate-questions",
+        {
+          role,
+          experience,
+          mode,
+          resumeText,
+          projects,
+          skills,
+        },
+        { withCredentials: true },
+      );
+      console.log(result.data);
+      if (userData) {
+        dispatch(
+          setUserData({ ...userData, credits: result.data.creditsLeft }),
+        );
+      }
+      setLoading(false);
+      onStart(result.data);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
     }
   };
   return (
@@ -208,12 +242,13 @@ function Step1SetUp({ onStart }) {
             )}
 
             <motion.button
-              disabled={!role || !experience}
+              onClick={handleStart}
+              disabled={!role || !experience || loading}
               whileHover={{ scale: 1.03 }}
               whileTap={{ scale: 0.95 }}
               className="w-full disabled:bg-gray-600 bg-green-600 hover:bg-green-700 text-white py-3 rounded-full text-lg font-semibold transition duration-300 shadow-md"
             >
-              Start Interview
+              {loading ? "Starting Interview..." : "Start Interview"}
             </motion.button>
           </div>
         </motion.div>
